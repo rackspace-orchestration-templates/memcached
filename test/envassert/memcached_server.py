93e0599bed21d555b33abb862b2b668716bdcdf9
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-06-26 18:06:20 -0500

    Adding initial tests

---
+++ test/envassert/memcached_server.py
@@ -0,0 +1,12 @@
+from fabric.api import env, task
+from envassert import detect, file, package, port, process, service
+
+@task
+def check():
+    env.platform_family = detect.detect()
+
+    assert package.installed("memcached")
+    assert file.exists("/etc/memcached.conf")
+    assert port.is_listening(11211)
+    assert process.is_up("memcached")
+    assert service.is_enabled("memcached")
