1dbd4d410cb9e647d6353bc07420b10a7920e991
Jason Boyles <jason@duncancreek.net>
2015-02-05 13:37:47 -0600

    add log artifact collection

--- test/fabric/memcached_port_11212.py
+++ test/fabric/memcached_port_11212.py
@@ -1,5 +1,6 @@
 from fabric.api import env, task
 from envassert import detect, file, package, port, process, service
+from hot.utils.test import get_artifacts, http_check
 
 
 @task
@@ -11,3 +12,9 @@ def check():
     assert port.is_listening(11212)
     assert process.is_up("memcached")
     assert service.is_enabled("memcached")
+
+
+@task
+def artifacts():
+    env.platform_family = detect.detect()
+    get_artifacts()
